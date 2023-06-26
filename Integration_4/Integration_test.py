c8dfe53bfc7f5b0fe74e308dc4c38a58182f4599
Reena-1205 <Rrani8490@conestogac.on.ca>
2023-06-26 15:36:11 -0400

    files added

--- Integration_4/Integration_test.py
+++ Integration_4/Integration_test.py
@@ -14,9 +14,11 @@ def test_integration():
 #
 # def test_subtraction():
 #     assert calculator.subtract(5, 3) == 2
-#
+
 # def test_multiplication():
+
 #     assert calculator.multiply(2, 3) == 6
+
 #
 # def test_division():
 #     assert calculator.divide(6, 3) == 2
